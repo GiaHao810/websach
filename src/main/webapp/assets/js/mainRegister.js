f101649f9d06133cd8675d8eefa390cf4ce60722
GiaHao810 <giahaobui810@gmail.com>
2023-03-14 18:09:11 +0700

    Sửa lại Form liên quan đến User

--- src/main/webapp/assets/js/mainRegister.js
+++ src/main/webapp/assets/js/mainRegister.js
@@ -4,13 +4,13 @@ var $$ = document.querySelectorAll.bind(document);
 // const loginContainer = $('.account-login__container')
 // const registerContainer = $('.account-register__container')
 // const createBtn = $('.account-create')
-const registerBtn = $('.btn-register')
+// const registerBtn = $('.btn-register')
 
 // createBtn.onclick = () => {
 //     loginContainer.style.display = 'none'
 //     registerContainer.style.display = 'block'
 // }
 
-registerBtn.onclick = () => {
-    window.location.href = 'login.html'
-}
+// registerBtn.onclick = () => {
+//     window.location.href = 'login.html'
+// }
